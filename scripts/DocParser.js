2ebc2efe5e66be80ad946449dd70e331b6a97703
Roy Sutton <roy.sutton@lge.com>
2017-01-02 03:45:53 -0800

    Tighten link converter, don't show undefined default
    Enact-DCO-1.1-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- scripts/DocParser.js
+++ scripts/DocParser.js
@@ -98,7 +98,7 @@ function copyStaticDocs (source, outputBase) {
 		shelljs.mkdir('-p', pathModule.normalize(outputPath));
 		if (ext === '.md') {
 			let contents = fs.readFileSync(file, 'utf8')
-				.replace(/index\.md/g, '')		// index files become 'root' for new directory
+				.replace(/(\((?!http)[^)]+)(\/index.md)/g, '$1/')		// index files become 'root' for new directory
 				.replace(/(\((?!http)[^)]+)(.md)/g, '$1/');			// other .md files become new directory under root
 			if (file.indexOf('index.md') === -1) {
 				contents = contents.replace(/\]\(\.\//g, '](../');	// same level .md files are now relative to root
