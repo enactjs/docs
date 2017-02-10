b284640583767da33965d5d646a5a6f7e45e6276
Dave Freeman <dave.freeman@lge.com>
2017-02-09 18:45:00 -0600

    add eslint-config-enact to package.json

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,29 +1,20 @@
 const shelljs = require('shelljs');
 shelljs.config.silent = true;
 
-// Clear out all parsed modules
-let dirs = shelljs.ls('-d', 'pages/docs/modules/*');
-dirs.forEach(dir => {
-	if (dir !== 'pages/docs/modules/index.js') {
-		shelljs.rm('-r', dir);
-	}
-});
+const leaveIndex = (dir, basePath = 'pages/docs/') => {
+	// remove everything but leave dir's index.js
+	const fullPath = basePath + dir;
+	const dirs = shelljs.ls('-d', fullPath + '/*');
+	dirs.forEach(dir => {
+		if (dir !== fullPath + '/index.js') {
+			shelljs.rm('-r', dir);
+		}
+	});
+};
 
-// Clear out all copied developer guide files (leave behind index.js)
-dirs = shelljs.ls('-d', 'pages/docs/developer-guide/*');
-dirs.forEach(dir => {
-	if (dir !== 'pages/docs/developer-guide/index.js') {
-		shelljs.rm('-r', dir);
-	}
-});
-
-// Clear out all copied developer tools files (leave behind index.js)
-dirs = shelljs.ls('-d', 'pages/docs/developer-tools/*');
-dirs.forEach(dir => {
-	if (dir !== 'pages/docs/developer-tools/index.js') {
-		shelljs.rm('-r', dir);
-	}
-});
+leaveIndex('modules');
+leaveIndex('developer-guide');
+leaveIndex('developer-tools');
 
 // Remove the public output directory
 shelljs.rm('-r', 'public');
