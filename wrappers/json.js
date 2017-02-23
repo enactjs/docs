5334a6d7ffa10baa50305d287af22c8f1176c916
Roy Sutton <pre101@pre101.com>
2017-02-22 20:12:52 -0500

    Feature/support see (#20)
    * PLAT-36488 Support @see
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Fix link text generation
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Fix className :genius:
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Index in right place
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Clean up link creation, probably
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Allow see tags in many places
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Remove 0 from function output
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Support constants and functions in module
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Add function to function header
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Add more types to header
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Clean up spotlight output directory
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Changes to how Classes, Functions, "See" references cleaned up.
    Added "warning" class and experimental angle rendering code.
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * Extract param types
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Emphasized the difference between params and returns blocks
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * Finish the @see design, by using a real color instead of a test one.
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- wrappers/json.js
+++ wrappers/json.js
@@ -1,6 +1,8 @@
-import React from 'react';
 import DocParse, {parseLink} from '../components/DocParse.js';
 import jsonata from 'jsonata';	// http://docs.jsonata.org/
+import {Link} from 'react-router';
+import {prefixLink} from 'gatsby-helpers';
+import React from 'react';
 
 const identifyType = (str) => {
 	if (str.indexOf('/') >= 0) {
@@ -9,50 +11,21 @@ const identifyType = (str) => {
 	return str ? str.toLowerCase().replace(/^.*\.(.+)$/, '$1') : '';
 };
 
-const renderModuleDescription = (doc) => {
-	if (doc.length) {
-		return <DocParse component="section" className="moduleDescription">{doc[0].description}</DocParse>;
-	}
-};
-
-const renderFunction = (func, index) => {
-	let params = func.params || [];
-	let paramStr = params.map((param) => (param.name)).join(', ');
-	let returnType = 'undefined';
-
-	if (func.returns && func.returns.length && func.returns[0].type && func.returns[0].type.name) {
-		returnType = func.returns[0].type.name;
-	}
-
-	return (
-		<section className="function" key={index}>
-			<dt>{func.name}({paramStr}) &rarr; {returnType}</dt>
-			<DocParse component="dd">{func.description}</DocParse>
-			<dd>
-				{params.length ? <h6>Params</h6> : null}
-				{params.length && params.map((param, subIndex) => (
-					<dl key={subIndex}>
-						<dt>{param.name}</dt>
-						<DocParse component="dd">{param.description}</DocParse>
-					</dl>
-					)
-				)}
-			</dd>
-			{returnType !== 'undefined' ? <dd>
-				<h6>Returns</h6>
-				<DocParse>{func.returns[0].description}</DocParse>
-			</dd> :
-			null}
-		</section>
-	);
-};
-
 const processTypeTag = (tags) => {
-	// First part extracts all `name` fields in `prop.tags` in the `type` member
+	// First part extracts all `name` fields in `tags` in the `type` member
 	// Null literal doesn't have a name field, so we need to see if one's there and append it to the
 	// list of all tag type names
 	const expression = "$append($[title='type'].**.name[],$[title='type'].**.$[type='NullLiteral'] ? ['null'] : [])";
 	const result = jsonata(expression).evaluate(tags);
+	return result || [];
+};
+
+const processParamTypes = (member) => {
+	// First part extracts all `name` fields in the `type` field of `member`
+	// Null literal doesn't have a name field, so we need to see if one's there and append it to the
+	// list of all tag type names
+	const expression = "$append($.type.**.name[],$.type.**.$[type='NullLiteral'] ? ['null'] : [])";
+	const result = jsonata(expression).evaluate(member);
 	return result || [];
 };
 
@@ -84,11 +57,128 @@ const hasHOCTag = (member) => {
 	return !!result;
 };
 
-const hasHOCConfigTag = (member) => {
-	// Find any tag field whose `title` is 'hocconfig'
-	const expression = "$[title='hocconfig']";
+const hasUITag = (member) => {
+	// Find any tag field whose `title` is 'ui'
+	const expression = "$[title='ui']";
 	const result = jsonata(expression).evaluate(member.tags);
 	return !!result;
+};
+
+const makeSeeLink = (tag, index) => {
+	// Parsing this will be difficult. http://usejsdoc.org/tags-see.html
+	let title = tag.description;
+	// Matching "{@link linkref|linkdesc} Extra text after"
+	const linkRegex = /{@link ([^| }]+)\|*([^}]*)}(.*)/;
+	// Matches non-link style module reference.  "moonstone/Module.Component.property Extra text"
+	// Currently doesn't require a '/' in the module name because of Spotlight but would be
+	// helpful, perhaps to require 'spotlight' or a slash
+	const moduleRegex = /([^.~ ]*)[.~]?(\S*)?(.*)/;
+	let linkText, link, res, extraText;
+
+	res = title.match(linkRegex);
+	if (res) {
+		title = link = res[1];
+		linkText = res[2] || title;
+		extraText = res[3];
+	} else {
+		linkText = link = title;
+	}
+
+	if (link.indexOf('http:') > -1) {
+		return <div className="see" key={index}>See <a href={title}>{linkText}</a>{extraText}</div>;
+	} else {
+		res = title.match(moduleRegex);
+		if (res) {	// Match is very permissive so this is safe bet
+			link = '/docs/modules/' + res[1] + '/';
+			if (res[2]) {
+				link += '#' + res[2];
+				title = res[1];
+			}
+			if (linkText === title) {
+				title = null;	// Don't have hover text if same as link text
+			}
+			extraText = extraText ? extraText + res[3] : res[3];
+		} else {	// Somehow, didn't match, just use text and no link?
+			link = null;
+			extraText = extraText ? title + extraText : title;
+		}
+
+		return <div className="see" key={index}>
+			See {link ? <Link to={prefixLink(link)} data-tooltip={title}>{linkText}</Link> : null}
+			{extraText}
+		</div>;
+	}
+};
+
+const getSeeTags = (member) => {
+	// Find any tag field whose `title` is 'see'
+	const expression = "$.tags[][title='see'][]";
+	return jsonata(expression).evaluate(member) || [];
+};
+
+const renderSeeTags = (member) => {
+	return getSeeTags(member).map((tag, idx) => {
+		return makeSeeLink(tag, idx);
+	});
+};
+
+const renderType = (type, index) => {
+	let typeContent = type;
+	if (typeContent.indexOf('/') >= 0) {
+		let shortText = typeContent.replace(/^.*\.(.+)$/, '$1');
+		typeContent = parseLink({children: [{text: shortText, value: typeContent}]});	// mapping to: child.children[0].value
+	}
+	return <span className={'type ' + identifyType(type)} key={index}>{typeContent}</span>;
+};
+
+const renderPropertyTypeStrings = (member) => {
+	const types = processTypeTag(member.tags);
+	const typeStr = types.map(renderType);
+	return typeStr;
+};
+
+const renderParamTypeStrings = (member) => {
+	const types = processParamTypes(member);
+	const typeStr = types.map(renderType);
+	return typeStr;
+};
+
+const renderFunction = (func, index) => {
+	let params = func.params || [];
+	let paramStr = params.map((param) => (param.name)).join(', ');
+	let returnType = 'undefined';
+
+	if (func.returns && func.returns.length && func.returns[0].type && func.returns[0].type.name) {
+		returnType = func.returns[0].type.name;
+	}
+
+	return (
+		<section className="function" key={index}>
+			<dt>{func.name}({paramStr}) &rarr; {returnType}</dt>
+			<DocParse component="dd">{func.description}</DocParse>
+			{(params.length || returnType !== 'undefined') ?
+				<dd className="details">
+					{params.length ? <div className="params">
+						<h6>{params.length} Param{params.length !== 1 ? 's' : ''}</h6>
+						{params.map((param, subIndex) => {
+							return (
+								<dl key={subIndex}>
+									<dt>{param.name} {renderParamTypeStrings(param)}</dt>
+									<DocParse component="dd">{param.description}</DocParse>
+								</dl>
+							);
+						})}
+					</div> : null}
+					{returnType !== 'undefined' ? <div className="returns">
+						<h6>Returns</h6>
+						<dl>
+							<dt>{renderType(returnType)}</dt>
+							<DocParse component="dd">{func.returns[0].description}</DocParse>
+						</dl>
+					</div> : null}
+				</dd> : null}
+		</section>
+	);
 };
 
 const renderProperty = (prop, index) => {
@@ -98,15 +188,6 @@ const renderProperty = (prop, index) => {
 		let isRequired = hasRequiredTag(prop.tags);
 		isRequired = isRequired ? <var className="required" data-tooltip="Required Property">&bull;</var> : null;
 
-		const types = processTypeTag(prop.tags);
-		const typeStr = types.map((type, idx) => {
-			let typeContent = type;
-			if (typeContent.indexOf('/') >= 0) {
-				let shortText = typeContent.replace(/^.*\.(.+)$/, '$1');
-				typeContent = parseLink({children: [{text: shortText, value: typeContent}]});	// mapping to: child.children[0].value
-			}
-			return <span className={'type ' + identifyType(type)} key={idx}>{typeContent}</span>;
-		});
 
 		let defaultStr = processDefaultTag(prop.tags);
 		defaultStr = defaultStr && defaultStr !== 'undefined' ? <var className="default"><span>Default: </span>{defaultStr}</var> : null;
@@ -117,10 +198,13 @@ const renderProperty = (prop, index) => {
 					{prop.name} {isRequired}
 				</dt>
 				<dd className="details">
-					{typeStr}
+					{renderPropertyTypeStrings(prop)}
 					{defaultStr}
 				</dd>
-				<DocParse component="dd" className="description">{prop.description}</DocParse>
+				<dd className="description">
+					<DocParse component="div">{prop.description}</DocParse>
+					{renderSeeTags(prop)}
+				</dd>
 			</section>
 		);
 	}
@@ -171,17 +255,56 @@ const renderInstanceProperties = (properties, isHoc) => {
 
 const renderModuleMember = (member, index) => {
 	const isHoc = hasHOCTag(member),
+		isFactory = hasFactoryTag(member),
+		isClass = (member.kind === 'class'),
+		isUI = hasUITag(member),
 		classes = 'module' +
-			(hasFactoryTag(member) ? ' factory' : '') +
-			(hasHOCTag(member) ? ' hoc' : '');
+			(isFactory ? ' factory' : '') +
+			(isHoc ? ' hoc' : '') +
+			(!isFactory && !isHoc && isClass ? ' class' : '');
 
-	// TODO: Check type for 'class' to filter out non-classes
-	return <section className={classes} key={index}>
-		<h4 id={member.name}>{member.name}</h4>
-		<div><DocParse>{member.description}</DocParse></div>
-		{renderStaticProperties(member.members, isHoc)}
-		{renderInstanceProperties(member.members, isHoc)}
-	</section>;
+	switch (member.kind) {
+		case 'function':
+			return <section className={classes + ' function'} key={index}>
+				<h4 id={member.name}>
+					{member.name}
+					{renderType('Function')}
+				</h4>
+				<dl>
+					{renderFunction(member)}
+				</dl>
+			</section>;
+		case 'constant':
+			return <section className={classes} key={index}>
+				<h4 id={member.name}>
+					{member.name}
+					{member.type ? renderType(member.type.name) : null}
+				</h4>
+				<div>
+					<DocParse>{member.description}</DocParse>
+					{renderSeeTags(member)}
+				</div>
+				{renderStaticProperties(member.members, isHoc)}
+				{renderInstanceProperties(member.members, isHoc)}
+			</section>;
+		case 'class':
+		default:
+			return <section className={classes} key={index}>
+				<h4 id={member.name}>
+					{member.name}
+					{renderType(isHoc ? 'Higher-Order Component' :	// eslint-disable-line no-nested-ternary
+						isFactory ? 'Component Factory' :	// eslint-disable-line no-nested-ternary
+						isUI ? 'Component' :
+						'Class')}
+				</h4>
+				<div>
+					<DocParse>{member.description}</DocParse>
+					{renderSeeTags(member)}
+				</div>
+				{renderStaticProperties(member.members, isHoc)}
+				{renderInstanceProperties(member.members, isHoc)}
+			</section>;
+	}
 };
 
 const renderModuleMembers = (members) => {
@@ -198,15 +321,23 @@ const renderModuleMembers = (members) => {
 	}
 };
 
+const renderModuleDescription = (doc) => {
+	if (doc.length) {
+		return <section className="moduleDescription">
+			<DocParse component="div" className="moduleDescriptionText">
+				{doc[0].description}
+			</DocParse>
+			{renderSeeTags(doc[0])}
+		</section>;
+	}
+};
+
 export default class JSONWrapper extends React.Component {
 
 	render () {
 		const doc = this.props.route.page.data;
-		const path = this.props.route.page.path.replace('/docs/modules/','').replace(/\/$/, "");;
+		const path = this.props.route.page.path.replace('/docs/modules/', '').replace(/\/$/, '');
 		// TODO: Just get this info from the doc itself?
-		if (!doc[0]) {
-			console.log(path);
-		}
 		return (
 			<div>
 				<h1>{path}</h1>
