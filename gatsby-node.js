9f42e51c5493e99b663cddf5a560103a96bdd786
Roy Sutton <roy.sutton@lge.com>
2017-05-06 00:39:46 -0400

    Update to ilib plugin
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,9 @@
 const ILibPlugin = require('ilib-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 exports.modifyWebpackConfig = function (config, stage) {
-	if (stage === 'build-html') {
+	//	if (stage === 'build-html') {
 		config.removeLoader('less');
 		config.removeLoader('css');
 		config.loader('css', function (cfg) {
@@ -11,7 +12,20 @@ exports.modifyWebpackConfig = function (config, stage) {
 						'css?-autoprefixer&modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]!postcss!less?sourceMap');
 			return cfg;
 		});
+		config.plugin('extract-text', ExtractTextPlugin, ['[name].css']);
 		config.plugin('ilib', ILibPlugin);
-	}
+	//}
+	config.merge({
+		postcss: [
+			autoprefixer({
+				browsers: [
+					'>1%',
+					'last 4 versions',
+					'Firefox ESR',
+					'not ie < 9' // React doesn't support IE8 anyway
+				]
+			})
+		]
+	});
 	return config;
 };
