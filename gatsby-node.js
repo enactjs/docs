8c741d0fa51397c3a777f98845e0b7b37ed0d810
Roy Sutton <roy.sutton@lge.com>
2017-05-07 02:57:29 -0400

    Fix all the things!
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,36 +3,41 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 
 exports.modifyWebpackConfig = function (config, stage) {
-	//	if (stage === 'build-html') {
-		config.removeLoader('less');
-		config.removeLoader('css');
-		config.loader('enact-css', function (cfg) {
-			cfg.test = /@enact.*\.(c|le)ss$/;
+	config.loader('css', cfg => {
+		cfg.exclude = /(enact\/.*|\.module).css$/;
+		return cfg;
+	});
+	config.loader('less', cfg => {
+		cfg.exclude = /(enact\/.*|\.module).less$/;
+		return cfg;
+	});
+	config.loader('enact-css', function (cfg) {
+		cfg.test = /enact\/.*\.(c|le)ss$/;
+		if (stage === 'develop') {
+			const cssModulesConf = 'css?modules&minimize&importLoaders=1';
+			const cssModulesConfDev = `${cssModulesConf}&sourceMap&localIdentName=[name]---[local]---[hash:base64:5]`;
+
+			cfg.loaders = ['style', cssModulesConfDev, 'less'];
+		} else {
 			cfg.loader = ExtractTextPlugin.extract('style',
 						'css?-autoprefixer&modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]!postcss!less?sourceMap');
-			return cfg;
-		});
-		config.loader('css', function (cfg) {
-			cfg.test = /\.(c|le)ss$/;
-			cfg.exclude = /@enact/;
-			cfg.loader = ExtractTextPlugin.extract('style',
-						'css?-autoprefixer&modules&sourceMap&importLoaders=1!postcss!less?sourceMap');
-			return cfg;
-		});
-	//		config.plugin('extract-text', ExtractTextPlugin, ['styles.css']);
-		config.plugin('ilib', ILibPlugin);
-	//}
-	config.merge({
-		postcss: function () { return [
-			autoprefixer({
-				browsers: [
-					'>1%',
-					'last 4 versions',
-					'Firefox ESR',
-					'not ie < 9' // React doesn't support IE8 anyway
-				]}
-			)
-		]}
+		}
+		return cfg;
 	});
+	config.merge({
+		postcss: function () {
+			return [
+				autoprefixer({
+					browsers: [
+						'>1%',
+						'last 4 versions',
+						'Firefox ESR',
+						'not ie < 9' // React doesn't support IE8 anyway
+					]}
+				)
+			];
+		}
+	});
+	config.plugin('ilib', ILibPlugin);
 	return config;
 };
