a2b10749ca2ab39b5802f9af2f17fee7d4b31c89
Roy Sutton <roy.sutton@lge.com>
2017-05-08 12:39:23 -0400

    First pass with react-live
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- pages/examples/button.js
+++ pages/examples/button.js
@@ -2,15 +2,37 @@ import Button from '@enact/moonstone/Button';
 import kind from '@enact/core/kind';
 import MoonstoneDecorator from '@enact/moonstone/MoonstoneDecorator';
 import React from 'react';
+import Spotlight from '@enact/spotlight';
+import {
+  LiveProvider,
+  LiveEditor,
+  LiveError,
+  LivePreview
+} from 'react-live'
 
-const app = kind({
-	name: 'Sample',
+const MoonstonePreview = MoonstoneDecorator(LivePreview);
 
-	render: (props) => (
-		<div {...props}>
-			<Button>Hello</Button>
-		</div>
-	)
-});
+const spotlightPause = () => { Spotlight.pause(); }
+const spotlightResume = () => { Spotlight.resume(); }
 
-export default MoonstoneDecorator(app);
+const app = () => (<LiveProvider code="<Button>Hello</Button>" scope={{React, Button, MoonstoneDecorator}}>
+  <LiveEditor onFocus={spotlightPause} onBlur={spotlightResume} tabIndex={-1} />
+  <LiveError />
+  <div style={{position: 'relative', minHeight: '200px', width: '100%'}}>
+	  <MoonstonePreview />
+  </div>
+</LiveProvider>);
+
+export default app;
+
+//const app = kind({
+	//name: 'Sample',
+
+	//render: (props) => (
+		//<div {...props}>
+			//<Button>Hello</Button>
+		//</div>
+	//)
+//});
+
+//export default MoonstoneDecorator(app);
