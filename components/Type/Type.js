f7f9f65d35e93ea0096114b633947bb0e3d5ee83
Blake Stephens <blake.stephens@lge.com>
2017-05-25 15:22:40 -0700

    Convert Type to a kind from a component
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- components/Type/Type.js
+++ components/Type/Type.js
@@ -2,6 +2,7 @@
 //
 import React from 'react';
 import PropTypes from 'prop-types';
+import kind from '@enact/core/kind';
 import {parseLink} from '../DocParse.js';
 
 import css from './Type.less';
@@ -13,8 +14,7 @@ const identifyType = (str) => {
 	return str ? str.toLowerCase().replace(/^.*\.(.+)$/, '$1') : '';
 };
 
-const readable = (content) => {
-	let typeContent = content;
+const readable = (typeContent) => {
 	if (typeContent.indexOf('/') >= 0) {
 		let shortText = typeContent.replace(/^.*\.(.+)$/, '$1');
 		typeContent = parseLink({children: [{text: shortText, value: typeContent}]});	// mapping to: child.children[0].value
@@ -22,15 +22,27 @@ const readable = (content) => {
 	return typeContent;
 };
 
-export default class Type extends React.Component {
-	static propTypes = {
-		children: PropTypes.string.isRequired
-	};
+const Type = kind({
+	name: 'Type',
 
-	render () {
-		const {children, ...rest} = this.props;
-		const typeContent = readable(children);
-		const typeClass = identifyType(children);
-		return <var {...this.props} className={'type ' + typeClass}>{typeContent}</var>;
-	}
-}
+	propTypes: {
+		children: PropTypes.string.isRequired
+	},
+
+	styles: {
+		css,
+		className: 'type'
+	},
+
+	computed: {
+		className: ({children, styler}) => styler.append(identifyType(children)),
+		children: ({children}) => readable(children)
+	},
+
+	render: (props) => (
+		<var {...props} />
+	)
+});
+
+export default Type;
+export {Type, identifyType, readable}
