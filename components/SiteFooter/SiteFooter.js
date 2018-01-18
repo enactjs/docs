08b1e13f5298a0871bdf359a8bd20c3ed1edd8a7
Roy Sutton <roy.sutton@lge.com>
2018-01-17 20:46:56 -0500

    Switch to Layout
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- components/SiteFooter/SiteFooter.js
+++ components/SiteFooter/SiteFooter.js
@@ -1,8 +1,9 @@
 // SiteFooter
 //
-import React from 'react';
-import {Link} from 'react-router';
 import kind from '@enact/core/kind';
+import {Link} from 'react-router';
+import React from 'react';
+import {Row, Cell} from '@enact/ui/Layout';
 
 import css from './SiteFooter.less';
 
@@ -23,11 +24,15 @@ const SiteFooterBase = kind({
 						<li><Link to="/legal/">Legal</Link></li>
 						<li><Link to="/contact/">Contact Us</Link></li>
 					</ul>
-					<ul className={css.social}>
-						<li><Link to="https://twitter.com/EnactJS">Twitter</Link></li>
-						<li><Link to="https://gitter.im/EnactJS/Lobby">Gitter</Link></li>
-					</ul>
-					<p className={css.copy}>Copyright &copy; 2017-2018 LG Electronics, Inc.</p>
+					<Row>
+						<Cell component="ul" className={css.social}>
+							<li><a href="https://twitter.com/EnactJS">Twitter</a></li>
+							<li><a href="https://gitter.im/EnactJS/Lobby">Gitter</a></li>
+						</Cell>
+						<Cell component="p" className={css.copy}>
+							Copyright &copy; 2017-2018 LG Electronics, Inc.
+						</Cell>
+					</Row>
 				</div>
 			</footer>
 		);
