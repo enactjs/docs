c28c7cfc2f7d495eaa3e5893a6de12e92ee2cf93
Dave Freeman <dave.freeman@lge.com>
2018-04-11 18:51:18 -0500

    add navigation anchors to headings

--- wrappers/md.jsx
+++ wrappers/md.jsx
@@ -4,6 +4,17 @@ import {config} from 'config'
 
 import css from '../css/main.less';
 
+const makeAnchorName = (headingText) => {
+    const anchor = headingText
+        .replace(/<.*>(.*)<\/.*>/g, '$1') // strip out inner tags
+        .replace('/', '-')
+        .replace(/[^A-Za-z0-9\- ]/g, '')
+        .replace(/\s+/g, '-');
+    return anchor.toLowerCase();
+};
+
+const headingMatch = /(<h(?!1)(\d)>)(.*)(<\/h\2>)/gim;
+
 module.exports = React.createClass({
   propTypes () {
     return {
@@ -11,13 +22,16 @@ module.exports = React.createClass({
     }
   },
   render () {
-    const post = this.props.route.page.data
+    const post = this.props.route.page.data;
+    const body = post.body.replace(headingMatch, (m, p1, p2, p3, p4) => {
+        return `${p1}<a name="${makeAnchorName(p3)}" class="navAnchor"></a>${p3}${p4}`;
+    });
 
     return (
       <DocumentTitle title={`${post.title} | ${config.siteTitle}`}>
         <div className={css.markdown}>
           <h1>{post.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: post.body }} />
+          <div dangerouslySetInnerHTML={{ __html: body }} />
         </div>
       </DocumentTitle>
     )
