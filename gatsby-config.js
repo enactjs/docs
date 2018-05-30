306a2378da7ccdd1dead36a0b18a0e54441a55b5
Roy Sutton <roy.sutton@lge.com>
2018-05-30 19:20:33 -0400

    Add autolink-headers plugin
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,8 @@ module.exports = {
 					},
 					'gatsby-remark-prismjs',
 					'gatsby-remark-copy-linked-files',
-					'gatsby-remark-smartypants'
+					'gatsby-remark-smartypants',
+					'gatsby-remark-autolink-headers'
 				]
 			}
 		},
