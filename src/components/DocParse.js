7fafb67ec75ecc686a3f55dc0b15ab08a001556e
Teck Liew <teck.liew@lge.com>
2018-07-31 17:20:08 -0700

    use child.url

--- src/components/DocParse.js
+++ src/components/DocParse.js
@@ -16,9 +16,12 @@ function parseCodeBlock (child, index) {
 function parseLink (child, index) {
 	let title = child.children[0].value;
 	const linkText = child.children[0].text || linkReference || title;
+	const url = child.url;
 
-	if (title.indexOf('http') === 0) {
-		return <OutboundLink href={title} key={index}>{linkText}</OutboundLink>;
+	if (url) {
+		return <OutboundLink href={url} key={index}>{linkText}</OutboundLink>;
+	} else if (title.indexOf('http') === 0) {
+		return <OutboundLink href={url} key={index}>{linkText}</OutboundLink>;
 	}
 	linkReference = null;
 	let pos = title.indexOf('.');
