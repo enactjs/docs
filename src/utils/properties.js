583b3c2b707ebe563195c7175a0c68c789d8cea7
Roy Sutton <roy.sutton@lge.com>
2018-10-21 22:25:22 -0400

    Add RecordType
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- src/utils/properties.js
+++ src/utils/properties.js
@@ -18,12 +18,12 @@ const processTypeTag = (tags) => {
 	// regular ones and combine TypeApplications (i.e. Arrays of type) into a single unit instead
 	// of having String[] render as ['String', 'Array'].  Then, it looks for 'NullLiteral' or
 	// 'AllLiteral' and replaces them with the word 'null' or 'Any'. If any 'StringLiteralType'
-	// exist, add them with quotes around the value.
+	// exist, add them with quotes around the value. A 'RecordType' is replaced with 'Object'.
 	// TODO: Add NumberLiteralType?
 	const expression = `$[title="type"].type.[(
 		$IsUnion := type = "UnionType";
 		$quote := function($val) { "'" & $val & "'" };
-		$GetNameExp := function($type) { $append($append($append($type[type="NameExpression"].name, $type[type="NullLiteral"] ? ['null'] : []), $type[type="AllLiteral"] ? ['Any'] : []), $map($type[type="StringLiteralType"].value, $quote)) };
+		$GetNameExp := function($type) { $append($append($append($append($type[type="NameExpression"].name, $type[type="NullLiteral"] ? ['null'] : []), $type[type="AllLiteral"] ? ['Any'] : []), $map($type[type="StringLiteralType"].value, $quote)), $type[type="RecordType"] ? ['Object'] : []) };
 		$GetType := function($type) { $type[type="TypeApplication"] ? $type[type="TypeApplication"].(expression.name & " of " & $GetNameExp(applications)[0]) : $type[type="OptionalType"] ? $GetAllTypes($type.expression) : $type[type="RestType"] ? $GetAllTypes($type.expression)};
 		$GetAllTypes := function($elems) { $append($GetType($elems), $GetNameExp($elems))};
 		$IsUnion ? $GetAllTypes($.elements) : $GetAllTypes($);
