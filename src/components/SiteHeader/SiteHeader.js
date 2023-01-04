5433720a12366fc03d4aecab62acdfae97e5dcdc
sjro <sj.ro@lge.com>
2023-01-04 18:39:06 +0900

    WRP-2861: Add a menu linking to enactjs.com/sampler (#197)
    * Add a menu linking to enactjs.com/sampler
    Enact-DCO-1.0-Signed-off-by: SJ RO (sj.ro@lge.com)
    
    * Update the menu name

--- src/components/SiteHeader/SiteHeader.js
+++ src/components/SiteHeader/SiteHeader.js
@@ -86,6 +86,13 @@ const SiteHeaderBase = kind({
 								>
 									Github
 								</Cell>
+								<Cell
+									component={OutboundLink}
+									shrink
+									href="https://enactjs.com/sampler"
+								>
+									UI Components
+								</Cell>
 							</Row>
 						</Cell>
 					</Row>
