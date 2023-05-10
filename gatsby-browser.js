016a84326498c1bbdedb879f12dc7ee76884a431
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2023-05-10 10:11:59 +0300

    fixed lint warning and updated travis.yml

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -23,9 +23,9 @@ export const onInitialClientRender = () => {
 			} else if (window.location.hash.includes('.')) {
 				// To select the element using `querySelector()` we escape the period in the ID with two backslashes
 				const parsedHashLocation = window.location.hash.replace(/\./g, '\\.');
-				const node = document.querySelector(parsedHashLocation);
+				const propNode = document.querySelector(parsedHashLocation);
 				if (node) {
-					node.scrollIntoView();
+					propNode.scrollIntoView();
 				}
 			} else {
 				console.log(`Invalid location: ${window.location.hash}`); // eslint-disable-line no-console
