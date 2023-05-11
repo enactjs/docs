5cc655c8fe87387e870d746d3a9123bcd4dd9edb
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2023-05-11 17:34:21 +0300

    small change to `onRouteUpdate`API

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,16 +4,17 @@ import 'prismjs/plugins/command-line/prism-command-line.css';
 export const onRouteUpdate = ({location}) => {
 	if (location.hash) {
 		setTimeout(() => {
-			let node = document.querySelector(`${location.hash}`);
-			if (node) {
-				node.scrollIntoView();
-			} else if (location.hash.includes('.')) {
+			let node;
+			if (location.hash.includes('.')) {
 				// To select the element using `querySelector()` we escape the period in the ID with two backslashes
 				const parsedHashLocation = location.hash.replace(/\./g, '\\.');
 				node = document.querySelector(parsedHashLocation);
-				if (node) {
-					node.scrollIntoView();
-				}
+			} else {
+				node = document.querySelector(`${location.hash}`);
+			}
+
+			if (node) {
+				node.scrollIntoView();
 			} else {
 				console.log(`Invalid location: ${location.hash}`);	// eslint-disable-line no-console
 			}
