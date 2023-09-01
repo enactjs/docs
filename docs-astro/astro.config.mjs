da1356572ec3f114625a8819de57e7d65e83cbe4
Andrei Tirla <andrei.tirla@lgepartner.com>
2023-09-01 15:17:17 +0300

    removed unnecessary configuration

--- docs-astro/astro.config.mjs
+++ docs-astro/astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
-global.__DEV__ = true;
-process.env.__DEV__=true;
-
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,10 +11,4 @@ export default defineConfig({
       experimentalReactChildren: true,
     }),
   ],
-  define: {
-    "__DEV__":  true,
-  },
-  optimizeDeps: {
-    include: ['@enact/**/*.css','@enact/**/*.jsx','@enact/**/*.js'],
-  },
 });
