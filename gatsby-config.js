cdf529acf452bdce919cac3da4bc4c367d259cea
taeyoung.hong <taeyoung.hong@lge.com>
2024-06-20 11:45:21 +0900

    add css-loader config for applying 6.x behavior

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,8 +78,11 @@ module.exports = {
 			resolve: `gatsby-plugin-postcss`,
 			options: {
 				cssLoaderOptions: {
+					// Options to restore 6.x behavior:
+					// https://github.com/webpack-contrib/css-loader/blob/master/CHANGELOG.md#700-2024-04-04
 					modules: {
-						namedExport: false
+						namedExport: false,
+						exportLocalsConvention: 'as-is'
 					}
 				}
 			}
