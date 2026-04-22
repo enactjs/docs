6a46ec0804a249a17d3742f281319668e61cb052
ion-andrusciac-lgp <ion.andrusciac@lgepartner.com>
2026-04-22 19:21:26 +0300

    Modified folders structures

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
+    outDir: './public',
+    publicDir: './static',
     vite: {
         resolve: {
             alias: {
