92c646825d64060b5093f3ad29533e997fbaefb5
ion-andrusciac-lgp <ion.andrusciac@lgepartner.com>
2026-06-19 18:39:59 +0300

    Added language for `highlight.js`

--- src/components/Code/Code.jsx
+++ src/components/Code/Code.jsx
@@ -1,10 +1,12 @@
 import hljs from 'highlight.js/lib/core';
+import xml from 'highlight.js/lib/languages/xml';
 
 import 'highlight.js/styles/vs2015.css';
 import css from './Code.module.css';
 
 const Code = ({children}) => {
 	if (typeof children === 'string') {
+		hljs.registerLanguage('xml', xml);
 		const highlight = hljs.highlight(children, {language: 'xml', ignoreIllegals: true});
 
 		return (
