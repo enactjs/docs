7f67eb784813f20621f19e8918279da748e87664
ion-andrusciac-lgp <ion.andrusciac@lgepartner.com>
2026-06-24 22:08:55 +0300

    Modified `make-runner`

--- scripts/make-runner.mjs
+++ scripts/make-runner.mjs
@@ -47,7 +47,7 @@ themes.forEach(theme => {
 		}
 
 		const ilibDst = `${dstBase}/ilib/locale`;
-		const ilibFiles = ['en/dateformats.json', 'en/sysres.json'];
+		const ilibFiles = ['en/localeinfo.json', 'en/dateformats.json', 'en/sysres.json'];
 		const copiedIlib = [];
 		for (const rel of ilibFiles) {
 			const src = `${srcBase}/ilib/locale/${rel}`;
