2a4c41e6ab237c8cad7927cb55d61345a7d57d45
ion-andrusciac-lgp <ion.andrusciac@lgepartner.com>
2026-06-26 22:39:36 +0300

    Removed check link from GitHub actions

--- scripts/prepare-raw.mjs
+++ scripts/prepare-raw.mjs
@@ -37,8 +37,8 @@ const args = parseArgs(process.argv);
 const rebuild = args['rebuild-raw'],
 	extraRepos = args['extra-repos'];
 
-copyGitHub('enactjs/enact', 'raw/enact', rebuild, 'feature/astro', args['ssh']);
-copyGitHub('enactjs/cli', 'raw/cli', rebuild, 'feature/astro', args['ssh']);
+copyGitHub('enactjs/enact', 'raw/enact', rebuild, args['enact-branch'], args['ssh']);
+copyGitHub('enactjs/cli', 'raw/cli', rebuild, args['cli-branch'], args['ssh']);
 copyGitHub('enactjs/eslint-config-enact', 'raw/eslint-config-enact', rebuild, args['eslint-config-branch'], args['ssh']);
 
 if (extraRepos) {
